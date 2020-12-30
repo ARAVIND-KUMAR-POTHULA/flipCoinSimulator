a064ead656b9c39e487acb62a504fd4bbfcc2570
ARAVIND-KUMAR-POTHULA <pothulaaravind241998@gmail.com>
2020-12-30 17:20:49 +0530

    ADD.added a file which displays  win or tie between heads and tails  after flipping a coin 21 times

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -3,9 +3,8 @@ heads=1
 tails=0
 headsCount=0
 tailsCount=0
-count=1
-read -p "enter a number how many times to flip a coin " multiple
-	while (( $count<=$multiple ))
+maxCount=21
+	while (( $headsCount<$maxCount &&  $tailsCount<$maxCount ))
 	do
 		flip=$(( RANDOM%2 ))
 		if (( $flip==$heads ))
@@ -14,7 +13,15 @@ read -p "enter a number how many times to flip a coin " multiple
 		else
 			((tailsCount++))
 		fi
-		((count++))
 	done
-echo "HEADS WON "$headsCount" times"
-echo "TAILS WON "$tailsCount" times"
+  if (( $headsCount==$tailsCount ))
+  then
+	echo "ITS TIE BETWEEN HEADS AND TAILS"
+  elif (( $headsCount>$tailsCount ))
+  then
+	diff=$((headsCount-tailsCount))
+	echo "HEADS WON "$diff" TIMES MORE THAN TAILS"
+  else
+	 diff=$((tailsCount-headsCount))
+        echo "TAILS WON "$diff" TIMES MORE THAN HEADS"
+  fi
