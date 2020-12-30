a87e33377c0c0789f1ac39a1e9c08ab28829b6db
ARAVIND-KUMAR-POTHULA <pothulaaravind241998@gmail.com>
2020-12-30 18:00:52 +0530

    ADD.added a file which simulates  2 points difference if its a tie between heads and tails

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -4,8 +4,9 @@ tails=0
 headsCount=0
 tailsCount=0
 maxCount=21
-	while (( $headsCount<$maxCount &&  $tailsCount<$maxCount ))
-	do
+	 while (( $headsCount<$maxCount &&  $tailsCount<$maxCount ))
+        do
+
 		flip=$(( RANDOM%2 ))
 		if (( $flip==$heads ))
 		then
@@ -14,14 +15,32 @@ maxCount=21
 			((tailsCount++))
 		fi
 	done
-  if (( $headsCount==$tailsCount ))
+
+  if (( $headsCount==$maxCount && $tailsCount==$maxCount ))
   then
 	echo "ITS TIE BETWEEN HEADS AND TAILS"
-  elif (( $headsCount>$tailsCount ))
+	diff=$(( headsCount-tailsCount ))
+	while (( $diff<2 ))
+	do
+		 flip=$(( RANDOM%2 ))
+		if ((  $flip==$heads ))
+  		then
+        		 ((headsCount++))
+			diff=$((headsCount-tailsCount))
+  		else
+         		 ((tailsCount++))
+			diff=$((tailsCount-headsCount))
+  		fi
+
+	done
+   fi
+  
+  if (( $headsCount>$tailsCount ))
   then
 	diff=$((headsCount-tailsCount))
 	echo "HEADS WON "$diff" TIMES MORE THAN TAILS"
   else
 	 diff=$((tailsCount-headsCount))
-        echo "TAILS WON "$diff" TIMES MORE THAN HEADS"
+         echo "TAILS WON "$diff" TIMES MORE THAN HEADS"
   fi
+
