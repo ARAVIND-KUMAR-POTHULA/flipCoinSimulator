ffe2a55f547542f7ff6e71a884b601d2bb54ce55
ARAVIND-KUMAR-POTHULA <pothulaaravind241998@gmail.com>
2020-12-30 16:46:02 +0530

    ADD.added a file which displays winner after flipping a coin

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,2 +1,10 @@
 #! /bin/bash/ -x
-echo "WELCOME TO FLIP COIN SIMULATOR"
+heads=1
+tails=0
+flip=$(( RANDOM%2 ))
+	if (( $flip==$heads ))
+	then
+		echo "HEADS IS WINNER"
+	else
+		 echo "TAILS IS WINNER"
+	fi
